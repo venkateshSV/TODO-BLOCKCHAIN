61f4a593c4954f7b3666d98953ec564204fe23d3
Venkatesh <venkatash.sv@gmail.com>
2021-01-06 17:15:39 +0530

    3.Create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,15 +10,32 @@ contract TodoList{
         bool completed;
     }
     mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+    );
+
+    event TaskCompleted(
+    uint id,
+    bool completed
+    );
+
     constructor() public{
         createTask("Plan Udaipur trip");
     }
 
-    function createTask(string memory _content) public{
+    function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
 
-        taskCount++;
-        tasks[taskCount] = Task(taskCount, _content,false);
-
-
-    }
+    function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 }
